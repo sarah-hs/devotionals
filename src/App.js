d2a01ba416b125a60c0a4d33b75ff1bb5622814b
sarah-hs <1170469@alumno.um.edu.mx>
2020-11-29 10:23:55 -0600

    tabs 1

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
+//import './App.css';
+//import axios from 'axios';
+//import { useEffect, useState } from 'react';
+//
+//function App() {
+//    const [devotional, setDevotional] = useState(undefined);
+//    const [isLoaded, setIsLoaded] = useState(false);
+//    const [error, setError] = useState(undefined);
+//
+//    useEffect(() => {
+//        axios.get('https://devotionals-api.herokuapp.com/api/v1/devotionals/today')
+//        .then(({data}) => setDevotional(data[0]))
+//        .catch((err) => setError(err))
+//        .finally(() => setIsLoaded(true));
+//    }, []);
+//
+//    if(error)
+//        return (
+//            <div className="App">
+//                <h1>An error ocurred</h1>
+//                <p>Error: {error.message}</p>
+//            </div>
+//        );
+//    else if(!isLoaded)
+//        return (
+//            <div className="App">
+//                <h1 className="loading">Loading...</h1>
+//            </div>
+//        );
+//    else
+//        return (
+//            <div className="App">
+//                <Devotional data={devotional} />
+//            </div>
+//        );
+//}
+//
+//function Devotional({data}) {
+//    return (
+//        <div className="devotional">
+//            <h1 className="title">{data.title}</h1>
+//            <p className="date">{data.date}</p>
+//            <p className="vers">{data.vers}</p>
+//            <div className="content">
+//                {data.content.map((p, i) => <p key={i}>{p}</p>)}
+//            </div>
+//        </div>
+//    )
+//}
+
+
+import React from 'react';
+import Tabs from "./components/Tabs";
 import './App.css';
-import axios from 'axios';
-import { useEffect, useState } from 'react';
 
 function App() {
-    const [devotional, setDevotional] = useState(undefined);
-    const [isLoaded, setIsLoaded] = useState(false);
-    const [error, setError] = useState(undefined);
-    
-    useEffect(() => {
-        axios.get('https://devotionals-api.herokuapp.com/api/v1/devotionals/today')
-        .then(({data}) => setDevotional(data[0]))
-        .catch((err) => setError(err))
-        .finally(() => setIsLoaded(true));
-    }, []);
-    
-    if(error)
-        return (
-            <div className="App">
-                <h1>An error ocurred</h1>
-                <p>Error: {error.message}</p>
-            </div>
-        );
-    else if(!isLoaded)
-        return (
-            <div className="App">
-                <h1 className="loading">Loading...</h1>
-            </div>
-        );
-    else
-        return (
-            <div className="App">
-                <Devotional data={devotional} />
-            </div>
-        );
-}
-
-function Devotional({data}) {
-    return (
-        <div className="devotional">
-            <h1 className="title">{data.title}</h1>
-            <p className="date">{data.date}</p>
-            <p className="vers">{data.vers}</p>
-            <div className="content">
-                {data.content.map((p, i) => <p key={i}>{p}</p>)}
-            </div>
-        </div>
-    )
+  return (
+      <div>
+        <h1>Tabs Demo</h1>
+          <Tabs>
+             <div label="Gator">
+               See ya later, <em>Alligator</em>!
+             </div>
+             <div label="Croc">
+               After 'while, <em>Crocodile</em>!
+             </div>
+             <div label="Sarcosuchus">
+               Nothing to see here, this tab is <em>extinct</em>!
+             </div>
+           </Tabs>
+      </div>
+  );
 }
 
 export default App;
