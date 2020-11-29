25b12b267d2752c28ac69dccfa52cb2823459843
sarah-hs <1170469@alumno.um.edu.mx>
2020-11-29 10:11:56 -0600

    load devotionals

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [devotional, setDevotional] = useState(undefined);
+    const [isLoaded, setIsLoaded] = useState(false);
+    const [error, setError] = useState(undefined);
+    
+    useEffect(() => {
+        axios.get('https://devotionals-api.herokuapp.com/api/v1/devotionals/today')
+        .then(({data}) => setDevotional(data[0]))
+        .catch((err) => setError(err))
+        .finally(() => setIsLoaded(true));
+    }, []);
+    
+    if(error)
+        return (
+            <div className="App">
+                <h1>An error ocurred</h1>
+                <p>Error: {error.message}</p>
+            </div>
+        );
+    else if(!isLoaded)
+        return (
+            <div className="App">
+                <h1 className="loading">Loading...</h1>
+            </div>
+        );
+    else
+        return (
+            <div className="App">
+                <Devotional data={devotional} />
+            </div>
+        );
+}
+
+function Devotional({data}) {
+    return (
+        <div className="devotional">
+            <h1 className="title">{data.title}</h1>
+            <p className="date">{data.date}</p>
+            <p className="vers">{data.vers}</p>
+            <div className="content">
+                {data.content.map((p, i) => <p key={i}>{p}</p>)}
+            </div>
+        </div>
+    )
 }
 
 export default App;
